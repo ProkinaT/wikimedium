6f8f80464e631581d95e75ed562e405a8f0b0d69
ProkinaT <tgprokina@edu.hse.ru>
2021-11-07 19:16:28 +0300

    Update scroll.js

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -1,15 +1,12 @@
-<script src="javascripts/jquery-3.6.0.js" charset="utf-8"></script>
-<script type="text/javascript">
-  $(function() {
-    $('.sidebar a').each(function() {
-      $(this).on('click', function(e) {
-        e.preventDefault();
-        var scrollTo = $(this).attr('href');
-        var whitespace = scrollTo == '#s1' ? 70 : 30;
-        $('html,body').animate({
-          scrollTop: $(scrollTo).offset().top - whitespace
-        }, 1000);
-      });
+$(function() {
+  $('.sidebar a').each(function() {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      var scrollTo = $(this).attr('href');
+      var whitespace = scrollTo == '#s1' ? 70 : 30;
+      $('html,body').animate({
+        scrollTop: $(scrollTo).offset().top - whitespace
+      }, 1000);
+     });
     });
-  });
-</script>
+    });
